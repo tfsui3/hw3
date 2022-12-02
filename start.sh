fd8189c3106bad99fc228b13f01aa8079ee82bd4
Student User <student@workstation.lab.example.com>
2022-12-02 00:19:39 -0500

    updates4

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 source /usr/local/etc/ocp4.config
 if [ -z ${RHT_OCP4_DEV_USER}]
-then
-  echo "Please login in OpenShift manually before starting"
-  echo "If already logged in OpenShift, just bash 'deploy.sh'"
+then	
+	echo "Please login in OpenShift manually before starting"
+  	echo "If already logged in OpenShift, just bash 'deploy.sh'"
 else
-  oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
-  bash deploy.sh
+  	oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+  	bash deploy.sh
+fi
