d7f2c0f174b2e7c22674608434b9707325babec0
tfsui3 <46233292+tfsui3@users.noreply.github.com>
2022-12-01 23:05:00 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 source /usr/local/etc/ocp4.config
-oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
-oc new-project zzy0064-hw3-deploy
-oc create -f quote-php-template.json
-oc process hw3-quote-php-persistent -p RHT_OCP4_QUAY_USER=zzy0064 | oc create -f -
-sleep 7
-oc expose svc quote-php
+if [ -z ${RHT_OCP4_DEV_USER}]
+then
+  echo "Please login in OpenShift manually before starting"
+  echo "If already logged in OpenShift, just bash 'deploy.sh'"
+else
+  oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+  bash deploy.sh
