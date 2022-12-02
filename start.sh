b852f0139a2b375c83dfe87157f9747219330a8c
tfsui3 <46233292+tfsui3@users.noreply.github.com>
2022-12-01 21:11:22 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 oc create -f quote-php-template.json
-oc process hw3-quote-php-persistent \
-> -p RHT_OCP4_QUAY_USER=zzy0064 \
->  | oc create -f -
-sleep(5)
+oc process hw3-quote-php-persistent -p RHT_OCP4_QUAY_USER=zzy0064 | oc create -f -
+sleep 5
 oc expose svc quote-php
