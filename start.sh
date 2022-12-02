c69901477f158d04497c6fe9ed1fb81707f71e2d
tfsui3 <46233292+tfsui3@users.noreply.github.com>
2022-12-01 22:55:52 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 source /usr/local/etc/ocp4.config
 oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
-oc new-project ${RHT_OCP4_DEV_USER}-deploy
+oc new-project zzy0064-hw3-deploy
 oc create -f quote-php-template.json
 oc process hw3-quote-php-persistent -p RHT_OCP4_QUAY_USER=zzy0064 | oc create -f -
 sleep 7
