b06c20a06e9bf95ce2eefeb541d40be0533b1520
Student User <student@workstation.lab.example.com>
2022-12-01 23:42:05 -0500

    updates3

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
+source /usr/local/etc/ocp4.config
+oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+oc new-project ${RHT_OCP4_DEV_USER}-deploy
 oc create -f quote-php-template.json
 oc process hw3-quote-php-persistent -p RHT_OCP4_QUAY_USER=zzy0064 | oc create -f -
-sleep 5
+sleep 7
 oc expose svc quote-php
